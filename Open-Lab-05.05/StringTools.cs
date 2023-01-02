d5161a71721e9e8b3e2679b73b08cd2fc82cd453
Roman <vojtus.roman@gmail.com>
2023-01-02 12:30:11 +0100

    ..

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -6,7 +6,32 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            char[] origchar = original.ToCharArray();
+            int i = 1;
+            int u = 0;
+            char[] newchar = new char[origchar.Length];
+            foreach(char c in origchar)
+            {
+                if (Char.IsWhiteSpace(c))
+                {
+                    newchar[u] = c;
+                    u++;
+                }
+                else if ( i % 2 == 0) 
+                {
+                    newchar[u] = Char.ToLower(c);
+                    u++;
+                    i++;
+                }
+                else if ( i % 2 != 0)
+                {
+                    newchar[u] = Char.ToUpper(c);
+                    u++;
+                    i++;
+                }
+            }
+            string result= new string(newchar);
+            return result;
         }
     }
 }
